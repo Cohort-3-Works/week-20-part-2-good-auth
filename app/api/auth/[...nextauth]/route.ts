be46fa4f9c7563dbfa17f5a64bb3797a5fbfe4db
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2024-12-27 17:10:01 +0530

    recapped till use session hook

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,36 @@
-import { NextRequest, NextResponse } from "next/server";
+import NextAuth, { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
 
-export function GET(req: NextRequest) {
-  return NextResponse.json({
-    message: "hi there from the route handler",
-  });
-}
+export const authOptions: AuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        username: {
+          label: "Username",
+          type: "text",
+          placeholder: "example@gmail.com",
+        },
+        password: {
+          label: "Password",
+          type: "password",
+        },
+      },
+      async authorize(credentials, req) {
+        // Replace this with actual logic to validate the user
+        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" };
+
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+};
+
+const handler = NextAuth(authOptions);
+
+export const GET = handler;
+export const POST = handler;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,40 @@
+"use client";
+import { SessionProvider, signIn, signOut, useSession } from "next-auth/react";
+
 export default function Home() {
   return (
+    <SessionProvider>
+      <OtherHome />
+    </SessionProvider>
+  );
+}
+
+function OtherHome() {
+  const session = useSession();
+
+  return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      you are finally logged in ..
+      {session.status == "authenticated" && (
+        <button
+          className="p-5 border bg-red-500 hover:bg-red-400 rounded-xl cursor-pointer  "
+          onClick={() => {
+            signOut();
+          }}
+        >
+          signOut
+        </button>
+      )}
+
+      {session.status == "unauthenticated" && (
+        <button
+          className="p-5 border bg-blue-500 hover:bg-blue-400 rounded-xl cursor-pointer"
+          onClick={() => {
+            signIn();
+          }}
+        >
+          signIn
+        </button>
+      )}
     </div>
   );
 }
